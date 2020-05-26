ca73fb585de615fff81d93e30bfbf62191cf148d
EthanQG <862089293@qq.com>
2020-05-26 17:52:11 +0800

    树的子结构

---
+++ jzoffer/src/test16/test1.java
@@ -0,0 +1,56 @@
+package test16;
+
+/**
+ * @Auther: wqg
+ * @Description:树的子结构
+ * 输入两棵二叉树A，B，判断B是不是A的子结构。（ps：我们约定空树不是任意一个树的子结构）
+ */
+public class test1 {
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+    }
+    public boolean HasSubtree(TreeNode root1,TreeNode root2) {
+        boolean result=false;
+        if (root1!=null&&root2!=null)
+        {
+            if (root1.val==root2.val) {
+                result = doesTree1HaveTree2(root1, root2);
+            }
+            if (!result) {
+                result = HasSubtree(root1.left, root2);
+            }
+            if (!result){
+                result=HasSubtree(root1.right,root2);
+            }
+
+        }
+        return result;
+    }
+    public static boolean doesTree1HaveTree2(TreeNode node1, TreeNode node2) {
+        //如果Tree2已经遍历完了都能对应的上，返回true
+        if (node2 == null) {
+            return true;
+        }
+        //如果Tree2还没有遍历完，Tree1却遍历完了。返回false
+        if (node1 == null) {
+            return false;
+        }
+        //如果其中有一个点没有对应上，返回false
+        if (node1.val != node2.val) {
+            return false;
+        }
+
+        //如果根节点对应的上，那么就分别去子节点里面匹配
+        return doesTree1HaveTree2(node1.left,node2.left) && doesTree1HaveTree2(node1.right,node2.right);
+    }
+
+
+
+}
