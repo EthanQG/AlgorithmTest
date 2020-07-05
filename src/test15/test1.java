d7482ab3c153d1cacc4320b776706aa1e0987daf
EthanQG <862089293@qq.com>
2020-07-05 11:39:16 +0800

    leetcode第102题 二叉树的层序遍历

--- src/test15/test1.java
+++ src/test15/test1.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 /**
  * @Auther: wqg
- * @Description:第102题 二叉树的层序遍历
+ * @Description:第102题二叉树的层序遍历
  * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 即逐层地，从左到右访问所有节点
  */
 public class test1 {
