c33127f2cb14deb82bf529f5229442deffdb3788
EthanQG <862089293@qq.com>
2020-07-05 11:37:37 +0800

    leetcode第98题 验证二叉树

--- src/test13/test1.java
+++ src/test13/test1.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 /**
  * @Auther: wqg
- * @Description:第98题 验证二叉搜索树
+ * @Description:第98题  验证二叉搜索树
     给定一个二叉树，判断其是否是一个有效的二叉搜索树。
     假设一个二叉搜索树具有如下特征：
     节点的左子树只包含小于当前节点的数。
